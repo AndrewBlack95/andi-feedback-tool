59a7028f10f6ac58d9a949140046e9594145f53c
Gavin Cook <gavin.cook@and.digital>
2021-09-28 12:21:26 +0100

    getting a login controller and associated test class into the project.

---
+++ src/test/java/com/and/digital/web/LoginControllerTest.java
@@ -0,0 +1,13 @@
+package com.and.digital.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @Test
+    void login_ReturnsLoginUrl() {
+        fail();
+    }
+}
