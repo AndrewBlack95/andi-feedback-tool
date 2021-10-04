86569f0bf027622677a49443a7caf1f9ad593611
Chris Hontoir <chrishontoir@Chriss-MacBook-Pro.local>
2021-10-04 08:58:40 +0100

    fix: remove mock api responses

--- src/front-end/src/api/get-survey-details.js
+++ src/front-end/src/api/get-survey-details.js
@@ -1,150 +1,3 @@
-// To be removed - useful for manual testing replace null in catch block with - new Promise(resolve => setTimeout(() => resolve(transform(mockGetSurveyDetailsResponse)), 3000))
-// const mockGetSurveyDetailsResponse = {
-//   surveyId: '12345',
-//   name: 'Week 1 Bootcamp Survey - Scotland',
-//   questionCount: 2,
-//   questions: [
-//     {
-//       title: 'Hi there',
-//       questionType: 'MULTIPLE_CHOICE',
-//       responses: []
-//     },
-//     {
-//       title: '<em>Hi there</em><br></br><h1>Hahaha</h1><br /><p>Hmmmm</p>',
-//       questionType: 'MULTIPLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 0
-//           },
-//           {
-//             value: 'N/A - All of the above',
-//             score: 0
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1>',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     },
-//     {
-
-//       title: '<em>Hi there</em></br><h1>Hahaha</h1><br />What is your Name?',
-//       questionType: 'SINGLE_CHOICE',
-//       responses: [
-//         [
-//           {
-//             value: 'N/A - All of the above',
-//             score: 2
-//           }
-//         ]
-//       ]
-//     }
-//   ]
-// }
-
 const transformOpenEnded = (responses) => {
   return responses.map(responseForPerson => {
     return { text: responseForPerson[0]?.value, score: null }
@@ -158,8 +11,8 @@ const transformSingleChoice = (responses) => {
     } else {
       return { text: null, score: responseForPerson[0]?.score }
     }
-  })
-}
+  });
+};
 
 const transformMultipleChoice = (responses) => {
   return responses.map(responseForPerson => {
@@ -167,20 +20,20 @@ const transformMultipleChoice = (responses) => {
       text: [ ...new Set(responseForPerson.map(response => response.value)) ].join(', '),
       score: null
     }
-  })
-}
+  });
+};
 
 const formatQuestionTitle = (title) => {
   const lineBreaks = /<br>/g;
   const endLineBreaks = /<\/?br ?\/?>/g;
   const htmlTags = /<.*?>/g;
   return title.replace(lineBreaks, '').replace(endLineBreaks, '. ').replace(htmlTags, '');
-}
+};
 
 const questionContainsName = (title) => {
   const name = /name/gi;
   return title.match(name)
-}
+};
 
 const transform = (data = {}) => {
  const { surveyId, name, questionCount, questions = [] } = data;
@@ -217,7 +70,7 @@ const transform = (data = {}) => {
     return acc;
    }, [])
  }
-}
+};
 
 const getSurveyDetails = (token, surveyId = '') => {
   return fetch(`http://localhost:8080/api/survey-details/${surveyId}`, { headers: { authorization: `Bearer ${token}` } })
--- src/front-end/src/api/get-surveys.js
+++ src/front-end/src/api/get-surveys.js
@@ -1,19 +1,3 @@
-// To be removed - useful for manual testing (replace empty array in catch block with this array)
-// const mockGetSurveysResponse = [
-//   { title: 'Week 1 Bootcamp Survey - Scotland', id: '12345' },
-//   { title: 'Week 1 Bootcamp Survey - The North', id: '12345' },
-//   { title: 'Week 1 Bootcamp Survey - The South', id: '12345' },
-//   { title: 'Week 2 Bootcamp Survey - Scotland', id: '12345' },
-//   { title: 'Week 2 Bootcamp Survey - The North', id: '12345' },
-//   { title: 'Week 2 Bootcamp Survey - The South', id: '12345' },
-//   { title: 'Week 3 Bootcamp Survey - Scotland', id: '12345' },
-//   { title: 'Week 3 Bootcamp Survey - The North', id: '12345' },
-//   { title: 'Week 3 Bootcamp Survey - The South', id: '12345' },
-//   { title: 'Week 4 Bootcamp Survey - Scotland', id: '12345' },
-//   { title: 'Week 4 Bootcamp Survey - The North', id: '12345' },
-//   { title: 'Week 4 Bootcamp Survey - The South', id: '12345' },
-// ];
-
 const validateResponse = (data) => {
   if (!Array.isArray(data)) {
     return [];
